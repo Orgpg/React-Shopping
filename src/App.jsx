ef5e5b49e4e0edebca70041dd88b5c304a25dfff
Orgpg24 <wpa5004@gmail.com>
2025-02-27 22:56:49 +0630

    add vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import { Analytics } from "@vercel/analytics/react";
 import CategorySection from "./Components/CategorySection";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import ProductSection from "./Components/ProductSection";
 
 export default function App() {
-
   return (
     <>
       <Header />
-      <CategorySection/>  
-      <ProductSection/>
+      <CategorySection />
+      <ProductSection />
       <Footer />
+      <Analytics />
     </>
   );
 }
